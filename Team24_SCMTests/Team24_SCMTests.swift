39972afdcb7d1b9268d122e50c4b09535e017286
harsh kumar <harsh7617chauhan@gmail.com>
2024-12-02 11:56:53 +0530

    Initial Commit

---
+++ Team24_SCMTests/Team24_SCMTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team24_SCMTests.swift
+//  Team24_SCMTests
+//
+//  Created by Batch - 1 on 02/12/24.
+//
+
+import Testing
+@testable import Team24_SCM
+
+struct Team24_SCMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
